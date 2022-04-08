021e53240770606fd9aa3efd590536d540c5128a
slava <osslav@mail.com>
2022-04-08 21:01:14 +0700

    create class TimerPaintWidget

--- task_2_9/timerpaintwidget.cpp
+++ task_2_9/timerpaintwidget.cpp
@@ -1,11 +1,20 @@
 #include "timerpaintwidget.h"
+#include <QTextCodec>
+#include <QBoxLayout>
 
-TimerPaintWidget::TimerPaintWidget(QWidget *parent)
+TimerPaintWidget::TimerPaintWidget(QWidget *parent)                             //конструктор виджета
     : QWidget(parent)
 {
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("Windows-1251"));    //установка кодека для локализации на русском
+    this->setWindowTitle("Обработка событий");                                  //установка заголовка окна
+    area = new Area(this);                                                      //создание виджета Area на текущем виджете
+    exitButton = new QPushButton("Завершить",this );                            //создание кнопки на текущем виджете
+    QVBoxLayout *layout = new QVBoxLayout(this);                                //создание вертикальной разметки на текущем виджете
+
+    layout->addWidget(area);                                                    //помещение на вертикальную разметку зону с фигурами и кнопку
+    layout->addWidget(exitButton);
+
+    connect(exitButton, SIGNAL(clicked(bool)),this,SLOT(close()));              //подключение сигнала нажатия кнопки к слоту закрытия окна
 }
 
-TimerPaintWidget::~TimerPaintWidget()
-{
-}
 
--- task_2_9/timerpaintwidget.h
+++ task_2_9/timerpaintwidget.h
@@ -2,13 +2,18 @@
 #define TIMERPAINTWIDGET_H
 
 #include <QWidget>
+#include "area.h"
+#include <QPushButton>
 
-class TimerPaintWidget : public QWidget
+class TimerPaintWidget : public QWidget             //виджет, содержащий виджет Area
 {
-    Q_OBJECT
+    Q_OBJECT                                        //подключение к классу сигналов и слотов
 
 public:
     TimerPaintWidget(QWidget *parent = nullptr);
-    ~TimerPaintWidget();
+
+protected:
+    Area * area;                                    // область отображения рисунка
+    QPushButton * exitButton;                       //кнопка закрытия виджета
 };
 #endif // TIMERPAINTWIDGET_H
