2d001bc21e98decfb3f23a8a150511f128d7752a
slava <osslav@mail.com>
2022-04-08 14:30:38 +0700

    add comments

--- task_2_8/counterwidget.h
+++ task_2_8/counterwidget.h
@@ -6,32 +6,32 @@
 #include <QLabel>
 #include <QPushButton>
 
-class CounterLine:public QLineEdit
+class CounterLine:public QLineEdit                              //класс виджета строки счетчика(наследуется от QLineEdit)
 {
-    Q_OBJECT
+    Q_OBJECT                                                    //макрос подключения сигналов и слотов к классу
 public:
-    CounterLine(const QString & contents, QWidget *parent=0):
+    CounterLine(const QString & contents, QWidget *parent=0):   //конструктор(с вызовом конструктора родительского класса)
     QLineEdit(contents,parent){}
 
 signals:
-    void multipleFiveSignal();
+    void multipleFiveSignal();                                  //сигнал, срабатывает когда счетчик хранит число кратное 5
 
 public slots:
-    void addOne();
+    void addOne();                                              //слот, добавляющий 1 к счетчику
 
 };
 
-class CounterWidget : public QWidget
+class CounterWidget : public QWidget                            //класс виджета для счетчика
 {
-    Q_OBJECT
+    Q_OBJECT                                                    //макрос подключения сигналов и слотов к классу
 
 public:
-    CounterWidget(QWidget *parent = nullptr);
+    CounterWidget(QWidget *parent = nullptr);                   //конструктор создания виджета
 
 private:
-    QLabel *labelCount,*labelCountByFive;
-    CounterLine *lineCount,*lineCountByFive;
-    QPushButton *addOneButton;
-    QPushButton *exitButton;
+    QLabel *labelCount,*labelCountByFive;                       //заголовки над счетчиками
+    CounterLine *lineCount,*lineCountByFive;                    //счетчики
+    QPushButton *addOneButton;                                  //кнопка добавления 1 к счетчику
+    QPushButton *exitButton;                                    //кнопка выхода
 };
 #endif // COUNTERWIDGET_H
